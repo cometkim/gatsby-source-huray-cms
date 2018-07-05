49daadd2dd6b6192029a633f9cd9fadae4214f15
Hyeseong Kim <cometkim.kr@gmail.com>
2018-07-05 11:43:45 +0900

    Cleanup code

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -2,11 +2,26 @@ import crypto from 'crypto'
 
 import HurayCmsClient from 'client'
 
+const buildGatsbyNode = ({ source, type, createNodeId }) => ({
+    ...source,
+    parent: 'root',
+    children: [],
+    id: createNodeId(`${type}_${source.id}`),
+    [`${type}Id`]: source.id,
+    internal: {
+        type: `Huray${type[0].toUpperCase()}${type.slice(1)}`,
+        contentDigest: crypto
+            .createHash('md5')
+            .update(JSON.stringify(source))
+            .digest('hex'),
+
+    },
+})
+
 exports.sourceNodes = async ({
     boundActionCreators: {
         createNode,
     },
-    reporter,
     createNodeId,
 }, {
     url,
@@ -17,77 +32,37 @@ exports.sourceNodes = async ({
     await client.login({ username, password })
 
     const users = await client.getAllUsers()
-    const userNodes = users
-    .map(user => {
-        const contentDigest = crypto
-            .createHash('md5')
-            .update(JSON.stringify(user))
-            .digest('hex')
-        return {
-            ...user,
-            parent: 'root',
-            children: [],
-            id: createNodeId(`user_${user.id}`),
-            userId: user.id,
-            internal: {
-                type: 'HurayUser',
-                contentDigest,
-            },
-        }
-    })
+    const userNodes = users.map(source => buildGatsbyNode({
+        type: 'user',
+        source,
+        createNodeId,
+    }))
     userNodes.forEach(node => createNode(node))
 
     const contents = await client.getAllContents()
-    const contentNodePromises = contents
-    .map(async content => {
-        const tags = content.tags || ['']
-        const fields = await client.getFieldsForContent({ contentId: content.id })
-
-        const attachments = await client.getAttachmentsForContent({ contentId: content.id })
-        const attachmentNodes = attachments
-        .map(attachment => {
-            const contentDigest = crypto
-                .createHash('md5')
-                .update(JSON.stringify(content))
-                .digest('hex')
-            return {
-                ...attachment,
-                parent: 'root',
-                children: [],
-                id: createNodeId(`attachment_${attachment.id}`),
-                attachmentId: attachment.id,
-                internal: {
-                    type: 'HurayAttachment',
-                    mediaType: attachment.content_type,
-                    contentDigest,
-                },
-            }
-        })
-        attachmentNodes.forEach(node => createNode(node))
-
-        const contentDigest = crypto
-            .createHash('md5')
-            .update(JSON.stringify(content))
-            .digest('hex')
-        return {
-            ...content,
-            parent: 'root',
-            children: [],
-            id: createNodeId(`content_${content.id}`),
-            customFields: fields,
-            tags,
-            contentId: content.id,
-            author___NODE: userNodes
-                .find(node => node.userId === content.author_id)
-                .id,
-            attachments___NODE: attachmentNodes.map(node => node.id),
-            internal: {
-                type: 'HurayContent',
-                mediaType: 'text/markdown',
-                contentDigest,
-            },
-        }
+    const contentNodes = contents.map(source => buildGatsbyNode({
+        type: 'content',
+        source,
+        createNodeId,
+    }))
+    contentNodes.forEach(node => node.internal.mediaType = 'text/markdown')
+    contentNodes.forEach(node => {
+        const authorNode = userNodes.find(userNode => node.author_id === userNode.userId)
+        node.author___NODE = authorNode.id
+        node.author_id = undefined
     })
-    const contentNodes = await Promise.all(contentNodePromises)
+    await Promise.all(
+        contentNodes.map(async node => {
+            const attachments = await client.getAttachmentsForContent({ contentId: node.contentId })
+            const attachmentNodes = attachments.map(source => buildGatsbyNode({
+                type: 'attachment',
+                source,
+                createNodeId,
+            }))
+            attachmentNodes.forEach(node => createNode(node))
+
+            node.attachments___NODE = attachmentNodes.map(node => node.id)
+        })
+    )
     contentNodes.forEach(node => createNode(node))
 } 
